3727a97c3e5759394466f991d6e54aed07aab9d0
Annu Ranjan <annur0710@gmail.com>
2018-08-04 20:39:51 +0530

    backing up

---
+++ aaa toDoList.tst
@@ -0,0 +1,4 @@
+1. Coloring all the buttons/providing theme to the site.
+2. Validating the passinggrades.
+3. Validating and filtering dates
+4. Route protection of emp details pages
--- src/app/admin/submenus/eim/education/education.component.ts
+++ src/app/admin/submenus/eim/education/education.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-education',
@@ -9,17 +9,41 @@ import { FormGroup } from '@angular/forms';
 export class EducationComponent implements OnInit {
 
   eduForm: FormGroup;
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.eduForm = this.fb.group({
+      education: this.fb.array([
+        this.getAnEducation()
+      ])
+    });
   }
 
+  getAnEducation() {
+    return this.fb.group({
+      qualification: ['', Validators.required],
+      schoolName: ['', Validators.required],
+      passingYear: ['', Validators.required],
+      passingGrade: ['', Validators.required]
+    });
+  }
+
+  addAnEducation() {
+    (<FormArray>this.eduForm.get('education')).push(this.getAnEducation());
+  }
+  deleteAnEducation(index: number) {
+    (<FormArray>this.eduForm.get('education')).removeAt(index);
+  }
+
+  onSubmit() {
+    console.log(this.eduForm);
+  }
 }
 
 
-education: [{
-  qualification: String,
-  schoolName: String,
-  passingYear: String,
-  passingGrade: String
-}]
+// education: [{
+//   qualification: String,
+//   schoolName: String,
+//   passingYear: String,
+//   passingGrade: String
+// }]
